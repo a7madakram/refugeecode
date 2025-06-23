5e4db8574846685f688f3d4706091a161168b8a4
Ahmed Akram <ahmedakram@Ahmeds-MacBook-Pro.local>
2025-06-23 23:41:49 +1000

    responsive web

--- js/listener.js
+++ js/listener.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll", function(){
     // var header = document.querySelector("header");
     document.body.classList.toggle("sticky", window.scrollY >50);
 });
+
+function myFunction() {
+    var x = document.getElementById("topNav");
+    if (x.className === "main-container-links") {
+      x.className += " responsive";
+    } else {
+      x.className = "main-container-links";
+    }
+  }
